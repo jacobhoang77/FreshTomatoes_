06f46ad479011a630bd4fc116181e07c758cd88c
jacobhoang77 <157303327+jacobhoang77@users.noreply.github.com>
2024-08-11 20:42:45 -0400

    Filter.js (Joseph did)

---
+++ src/pages/Filter.js
@@ -0,0 +1,14 @@
+export const filterMoviesByCategory = (movies, category) => {
+    switch (category) {
+      case 'BoxOffice':
+        return movies.filter(movie => movie.boxOffice);
+      case 'InTheaters':
+        return movies.filter(movie => movie.inTheaters);
+      case 'Opening':
+        return movies.filter(movie => movie.opening);
+      case 'Upcoming':
+        return movies.filter(movie => movie.upcoming);
+      default:
+        return movies;
+    }
+  };
